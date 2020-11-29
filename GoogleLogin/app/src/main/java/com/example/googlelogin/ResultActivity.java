faa16ebfcc16e5b9785aa4454bf231841540074a
Minjaee <chelsea@jbnu.ac.kr>
2020-11-29 18:42:47 +0900

    로그인밖에 안됨

---
+++ GoogleLogin/app/src/main/java/com/example/googlelogin/ResultActivity.java
@@ -0,0 +1,36 @@
+package com.example.googlelogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class ResultActivity extends AppCompatActivity {
+
+    private TextView tv_result; // 닉네임 text
+    private ImageView iv_profile; // 이미지 뷰
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+
+        Intent intent = getIntent();
+        String nickName = intent.getStringExtra("nickName"); // MainActivity로 부터 닉네임 전달받음
+        String photoUrl = intent.getStringExtra("PhotoUrl"); // MainActivity로 부터 프로필사진 url 전달받음
+
+        tv_result = findViewById(R.id.tv_result);
+        tv_result.setText(nickName); // 닉네임 텍스트를 텍스트뷰에 세팅.
+
+
+        iv_profile = findViewById(R.id.iv_profile);
+        Glide.with(this). load(photoUrl). into(iv_profile); // 프사 url을 이미지 뷰에 세팅
+
+
+    }
+}
